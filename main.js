c625649b7981d43f59641689a35341662138316d
andriy-lanyk <LAM1988@ukr.net>
2021-07-09 00:43:33 +0300

    Fixed enter name

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import wishes from "./wishes.js";
+import wishes from './wishes.js';
 
 const time = document.querySelector('.time');
 const date = document.querySelector('.date');
@@ -7,96 +7,128 @@ const greetingName = document.querySelector('.greeting-name');
 const wish = document.querySelector('.wish');
 
 function showTime() {
-    let today = new Date();
-    let hour = today.getHours();
-    let minutes = today.getMinutes();
-    let seconds = today.getSeconds();
-    
-    time.textContent = `${addZero(hour)}:${addZero(minutes)}:${addZero(seconds)}`;
+  let today = new Date();
+  let hour = today.getHours();
+  let minutes = today.getMinutes();
+  let seconds = today.getSeconds();
 
-    setTimeout(showTime, 1000);
-};
+  time.textContent = `${addZero(hour)}:${addZero(minutes)}:${addZero(seconds)}`;
+
+  setTimeout(showTime, 1000);
+}
 
 showTime();
 
 function setDate() {
-    let today = new Date();
-    let day = today.getDate();
-    let month = `${today.getMonth() + 1}`;
-    let year = today.getFullYear();
+  let today = new Date();
+  let day = today.getDate();
+  let month = `${today.getMonth() + 1}`;
+  let year = today.getFullYear();
 
-    const getWeekDay = (today) => {
-        let days = ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'];
-        
-        return days[today.getDay()];
-    }
-    
-    date.textContent = `${getWeekDay(today)} ${addZero(day)}.${addZero(month)}.${year}`;
+  const getWeekDay = today => {
+    let days = [
+      'Воскресенье',
+      'Понедельник',
+      'Вторник',
+      'Среда',
+      'Четверг',
+      'Пятница',
+      'Суббота',
+    ];
+
+    return days[today.getDay()];
+  };
+
+  date.textContent = `${getWeekDay(today)} ${addZero(day)}.${addZero(
+    month,
+  )}.${year}`;
 }
 
 setDate();
 
-function addZero(number) { return number < 10 ? `0${number}` : `${number}` };
+function addZero(number) {
+  return number < 10 ? `0${number}` : `${number}`;
+}
 
 function greeting() {
-    let today = new Date();
-    let hour = today.getHours();
-    if (hour >= 5 && hour < 11) {
-        greetingText.textContent = 'Доброе утро';
-        document.body.style.backgroundImage = "url('./img/Morning.jpg')";
-        document.body.style.color = "Maroon";
-    } else if (hour >= 11 && hour < 18) {
-        greetingText.textContent = 'Добрый день';
-        document.body.style.backgroundImage = "url('./img/Afternoon.jpg')";
-        document.body.style.color = "Yellow";
-    } else if (hour >= 18 && hour < 22) {
-        greetingText.textContent = 'Добрый вечер';
-        document.body.style.background = "linear-gradient( rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4) ), url('./img/Evening.jpg')";
-        document.body.style.backgroundSize = "cover";
-        document.body.style.color = "OrangeRed";
-    } else {
-        greetingText.textContent = 'Доброй ночи';
-        document.body.style.backgroundImage = "url('./img/Night.jpg')";
-        document.body.style.color = "LightGrey";
-    }
+  let today = new Date();
+  let hour = today.getHours();
+  if (hour >= 5 && hour < 11) {
+    greetingText.textContent = 'Доброе утро';
+    document.body.style.backgroundImage = "url('./img/Morning.jpg')";
+    document.body.style.color = 'Maroon';
+  } else if (hour >= 11 && hour < 18) {
+    greetingText.textContent = 'Добрый день';
+    document.body.style.backgroundImage = "url('./img/Afternoon.jpg')";
+    document.body.style.color = 'Yellow';
+  } else if (hour >= 18 && hour < 22) {
+    greetingText.textContent = 'Добрый вечер';
+    document.body.style.background =
+      "linear-gradient( rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4) ), url('./img/Evening.jpg')";
+    document.body.style.backgroundSize = 'cover';
+    document.body.style.color = 'OrangeRed';
+  } else {
+    greetingText.textContent = 'Доброй ночи';
+    document.body.style.backgroundImage = "url('./img/Night.jpg')";
+    document.body.style.color = 'LightGrey';
+  }
 }
 
 greeting();
 
-greetingName.addEventListener("keypress", setName);
-greetingName.addEventListener("blur", setName);
+greetingName.addEventListener('keypress', setName);
+greetingName.addEventListener('blur', setName);
+greetingName.addEventListener('click', changeGreetingNameText);
 
-function getName () {
-    if (localStorage.getItem('name') === null || localStorage.getItem('name') === "") {
-        greetingName.textContent = '[введите свое имя]';
-    } else {
-        greetingName.textContent = `${localStorage.getItem('name')}!`;
-    }
-};
+function changeGreetingNameText() {
+  greetingName.textContent = ' ';
+  greetingName.style.width = '10px';
+  wish.textContent = '[желание появиться когда я узнаю Ваше имя]';
+  setTimeout(function () {
+    greetingName.focus();
+  }, 0);
+}
 
-function setName (event) {
-    if (event.type === 'keypress') {
-        if (event.which == 13 || event.keyCode == 13) {
-        localStorage.setItem('name', event.target.innerText);
-        greetingName.blur();
-        }    
-    } else {
-        localStorage.setItem('name', event.target.innerText)
+function getName() {
+  if (
+    localStorage.getItem('name') === null ||
+    localStorage.getItem('name') === ''
+  ) {
+    greetingName.textContent = '[введите свое имя]';
+    wish.textContent = '[желание появиться когда я узнаю Ваше имя]';
+  } else {
+    greetingName.textContent = `${localStorage.getItem('name')}!`;
+  }
+}
+
+function setName(event) {
+  if (event.type === 'keypress') {
+    greetingName.style.width = '';
+    if (event.which == 13 || event.keyCode == 13) {
+      localStorage.setItem('name', event.target.innerText);
+      greetingName.blur();
+      printWish();
     }
-};
+  } else {
+    localStorage.setItem('name', event.target.innerText);
+  }
+}
 
 getName();
 
-function choiceOfWishes() { return [...wishes][Math.floor(Math.random() * (([...wishes].length) - 0 + 1)) + 0].toLowerCase() };
+function choiceOfWishes() {
+  return [...wishes][
+    Math.floor(Math.random() * ([...wishes].length - 0 + 1)) + 0
+  ].toLowerCase();
+}
 
 function printWish() {
-    wish.textContent = ` ${choiceOfWishes()}`;
-    wish.style.textDecoration = "underline";
+  wish.textContent = ` ${choiceOfWishes()}`;
+  wish.style.textDecoration = 'underline';
 
-    setTimeout(printWish, 60000);
-};
+  setTimeout(printWish, 30000);
+}
 
 if (localStorage.getItem('name')) {
-    console.log(localStorage.getItem('name'));
-    printWish();
-};
+  printWish();
+}
